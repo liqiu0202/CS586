7ea6260ba34177460ac13a3c7ff1f7b7459a497b
Weiwei <heymrbox@gmail.com>
2015-04-11 18:58:59 -0700

    New update

---
+++ UI/js/javascript.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+	$("#accordian a").click(function(){
+		alert("entered as well");
+	})
+
+
+
+
+
+
+	$("#accordian h3").click(function(){
+		//slide up all the link lists
+		$("#accordian ul ul").slideUp();
+		//slide down the link list below the h3 clicked - only if its closed
+		if(!$(this).next().is(":visible"))
+		{
+			$(this).next().slideDown();
+		}
+	})
+})

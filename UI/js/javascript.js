625f30a6e5f9829eb1c561596f93715763cd5373
Weiwei <heymrbox@gmail.com>
2015-04-12 23:22:13 -0700

    Still in configuration

--- UI/js/javascript.js
+++ UI/js/javascript.js
@@ -1,15 +1,5 @@
-
 $(document).ready(function(){
-	$("#accordian a").click(function(){
-		alert("entered as well");
-	})
-
-
-
-
-
-
-	$("#accordian h3").click(function(){
+    $("#accordian h3").click(function () {
 		//slide up all the link lists
 		$("#accordian ul ul").slideUp();
 		//slide down the link list below the h3 clicked - only if its closed
@@ -17,5 +7,64 @@ $(document).ready(function(){
 		{
 			$(this).next().slideDown();
 		}
+		event.preventDefault();
+    	return false;
+    })
+	var url = "http://localhost:8080/examples/servlets/servlet/QueryProcessor?target=";
+
+	$("#accordian a").on("click", function (e) {
+		    var elementId = $(this).attr("id");
+		    url += document.getElementById(elementId).text;
+		    sendRequest(url);
+	 //    $.ajax({
+		// 	type: "GET",
+		// 	url:'http://localhost:8080/examples/servlets/servlet/QueryProcessor',
+		// 	data: "?target=" + param,
+  //          	//dataType:'json',
+  //          	success: function(response){
+  //          		alert("aa");
+		// 	}
 	})
-})
+});
+
+function sendRequest(url) {
+	alert(url);
+	req = null;
+	if( window.XMLHttpRequest ){
+		req = new XMLHttpRequest();
+	}else if( window.ActiveObject ){
+		try{
+			req = new ActiveObject("Microsoft.XMLHTTP");
+		}catch( e ){
+		}
+	}
+	if( req ){
+		req.open("GET", url, false);
+		req.onreadystatechange = showResult;
+		req.setRequestHeader("Connection", "Close");
+		req.setRequestHeader("Method", "GET" + url + "HTTP/1.1" );
+		req.send();
+	}else{
+		alert("Sorry, but I couldn't create and XMLHttpRequest");
+	}
+}
+
+function showResult(){
+	alert("show");
+	var result = document.getElementById('result');
+	result.innerHTML = "aaa";
+	if( req.readyState == 4  && req.status == 200 ){
+		var str = "<ul>";
+		var jsonData = JSON.parse( req.responseText );
+		results = jsonData;
+		for(var i in results ){
+			var link = results[i].wikiLink;
+			var name = results[i].name;
+			str += "<li><a href='" + link + "' target='_blank'>" + name + "</a></li>";
+		}
+		str += "</ul>";
+		var result = document.getElementById('result');
+		result.innerHTML = str;
+	}
+	
+}
